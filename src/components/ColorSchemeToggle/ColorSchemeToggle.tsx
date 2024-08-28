1e63dd89fa462afce5c1df952301f3d25400260d
Michael Bossner <michael.bossner@hotmail.com>
2024-08-28 23:31:28 +1000

    v1.2
    updated the button to better reflect the theme change

--- src/components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ src/components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,13 +1,15 @@
 import * as React from "react";
 import { ActionIcon, useMantineColorScheme } from "@mantine/core";
 import { IconSun } from "@tabler/icons-react";
+import { IconMoon } from "@tabler/icons-react";
 
 export function ColorSchemeToggle() {
-  const { toggleColorScheme } = useMantineColorScheme();
+  const { toggleColorScheme, colorScheme } = useMantineColorScheme();
+  const icon = colorScheme === "dark" ? <IconSun /> : <IconMoon />;
 
   return (
     <ActionIcon size="lg" variant="outline" onClick={() => toggleColorScheme()}>
-      <IconSun />
+      {icon}
     </ActionIcon>
   );
 }
